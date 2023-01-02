34a064631edbeef364d4dc20c0e4998d5d068a8a
chisator <gianfrancosacchi@hotmail.com.ar>
2023-01-01 21:04:50 -0300

    Agregue parte del login y cambie la carpeta de 'imagenes' a 'img'

--- Codigo/app.js
+++ Codigo/app.js
@@ -8,3 +8,4 @@ app.listen(PORT,()=>console.log('Servidor corriendo en puerto '+ PORT))
 app.use(express.static(path.resolve(__dirname,'./public')));
 
 app.get('/',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/index.html')))
+app.get('/login',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/login.html')))
