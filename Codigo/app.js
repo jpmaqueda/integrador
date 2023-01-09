f220cfeefc7484df609f60c39d8df67fcc74deb6
sebapitta <sebapitta@gmail.com>
2023-01-09 13:15:03 -0300

    agregué el html y css del shoppingCart

--- Codigo/app.js
+++ Codigo/app.js
@@ -12,3 +12,6 @@ app.get('/',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/index.html'
 app.get('/login',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/login.html')))
 
 app.get('/productDetail',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/productDetail.html')))
+
+app.get('/shoppingCart',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/shoppingCart.html')))
+
