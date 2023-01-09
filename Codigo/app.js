649dec69205cc3c6fa4a8086cf04fa27f7264188
SebaGarea <sebastiangarea@outlook.com>
2023-01-09 11:53:44 -0300

    agregue ProductDetail.html, product-Detail-Styles.css

--- Codigo/app.js
+++ Codigo/app.js
@@ -8,4 +8,7 @@ app.listen(PORT,()=>console.log('Servidor corriendo en puerto '+ PORT))
 app.use(express.static(path.resolve(__dirname,'./public')));
 
 app.get('/',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/index.html')))
+
 app.get('/login',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/login.html')))
+
+app.get('/productDetail',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/productDetail.html')))
