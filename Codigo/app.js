fdd6c2450302e57811fbd7de0858a34ee352c830
chisator <gianfrancosacchi@hotmail.com.ar>
2023-01-13 21:13:25 -0300

    Subiendo css y html de login y register

--- Codigo/app.js
+++ Codigo/app.js
@@ -9,3 +9,4 @@ app.use(express.static(path.resolve(__dirname,'./public')));
 
 app.get('/',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/index.html')))
 app.get('/login',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/login.html')))
+app.get('/register',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/register.html')))
