a3c5ab290878862c8d2f758337643800a19a1e4b
jpmaqueda <jpmaqueda@gmail.com>
2023-01-18 20:10:00 +0000

    adding scripts to json

--- Codigo/app.js
+++ Codigo/app.js
@@ -1,20 +1,30 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const path=require('path')
-const PORT=3000;
+const path = require("path");
+const PORT = 3000;
 
-app.listen(PORT,()=>console.log('Servidor corriendo en puerto '+ PORT))
+app.listen(PORT, () => console.log("Servidor corriendo en puerto " + PORT));
 
-app.use(express.static(path.resolve(__dirname,'./public')));
+app.use(express.static(path.resolve(__dirname, "./public")));
 
-app.get('/',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/index.html')))
+app.get("/", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/index.html"))
+);
 
-app.get('/register',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/register.html')))
+app.get("/register", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/register.html"))
+);
 
-app.get('/login',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/login.html')))
+app.get("/login", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/login.html"))
+);
 
-app.get('/productDetail',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/productDetail.html')))
+app.get("/productDetail", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/productDetail.html"))
+);
 
-app.get('/shoppingCart',(req,res)=> res.sendFile(path.resolve(__dirname,'./views/shoppingCart.html')))
+app.get("/shoppingCart", (req, res) =>
+  res.sendFile(path.resolve(__dirname, "./views/shoppingCart.html"))
+);
 
-
+app.set("view engine", "ejs");
