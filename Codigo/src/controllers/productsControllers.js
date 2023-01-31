449c6a8541efd44e0fdf4ceb97876df02c2eb654
jpmaqueda <jpmaqueda@gmail.com>
2023-01-31 00:35:46 +0000

    subiendo rutas de creacion de formulario

--- Codigo/src/controllers/productsControllers.js
+++ Codigo/src/controllers/productsControllers.js
@@ -1,10 +1,39 @@
+const fs = require(`fs`)
+const path = require(`path`)
+const filepath = path.join(__dirname, "../data/productsDataBase.json")
+
+const productslectura = JSON.parse(fs.readFileSync(filepath, `utf-8`))
+
 const pcControllers={
     create:(req,res)=> res.render('productcreation'),
     edit: (req, res) => res.render("productedit"),
     detail:(req,res)=> res.render('productDetail'),
-    cart:(req,res)=> res.render('shoppingCart')
+    cart:(req,res)=> res.render('shoppingCart'),
+    productadmin:(req,res)=> res.render('productsadmin', {productslectura}),
+    store:(req,res)=> {
+        const productregister=getProductList(filepath) // esto sirve para para mostrar el json creado sin tener que hacer refresh//
+        const datosproducto={
+            id: productregister.length > 0 ? productregister[productregister.length -1].id + 1 : 1,
+            nombre: req.body.nombre,
+            precio: req.body.precio,
+            stock: req.body.stock,
+            color: req.body.color,
+            altura: req.body.altura,
+            profundidad: req.body.profundidad,
+            anchura: req.body.anchura,
+            }
+            productregister.push(datosproducto)
+         
+            fs.writeFileSync(filepath, JSON.stringify(productregister, null, 2));
+            return res.redirect("/productsadmin")
+
+        
+    }
 
     
+}
+function getProductList(path) {
+	return JSON.parse(fs.readFileSync(path, 'utf-8'));
 }
 
 module.exports=pcControllers
--- Codigo/src/routes/products.js
+++ Codigo/src/routes/products.js
@@ -1,10 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const pcControllers = require("../controllers/productsControllers");
+const path =  require("path")
+const multer = require("multer")
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, path.join(__dirname, "../../public/imagenes/productos"));
+    },
+    filename: function (req, file, cb) {
+      const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+      cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+    }
+  })
+  const upload = multer({ storage: storage })
 
-router.get("/productcreation", pcControllers.create);
+
+router.get("/productcreation",pcControllers.create);
+router.post("/store", upload.single("img") ,pcControllers.store);
 router.get("/productedit", pcControllers.edit);
 router.get('/productDetail',pcControllers.detail)
 router.get('/shoppingCart', pcControllers.cart);
+router.get('/productsadmin', pcControllers.productadmin);
 
 module.exports = router;
