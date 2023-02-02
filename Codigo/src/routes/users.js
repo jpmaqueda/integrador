8b2a657bb059a8cad6b6709c83653c7b1c594393
chisator <gianfrancosacchi@hotmail.com.ar>
2023-02-02 18:12:09 -0300

    usando middleware en register

--- Codigo/src/routes/users.js
+++ Codigo/src/routes/users.js
@@ -1,14 +1,28 @@
 const express=require('express');
 const router=express.Router();
-const registerControllers=require('../controllers/registerControllers');
+const usersControllers=require('../controllers/UsersControllers');
+const {body}=require('express-validator')
+const validation=[
+    body('nombre').notEmpty().withMessage('Escribe tu nombre y apellido.'),
+
+    body('email').isEmail().withMessage('ingresa un Email válido.'),
+
+    body('contrasena').notEmpty().withMessage('Al menos 8 caracteres.'),
+
+    body('contrasena2').notEmpty().withMessage('Las contraseñas no son iguales.'),
+];
+//fileName = `${Date.now()}-img${path.extname(file.originalname)};`
+
+router.get('/register', usersControllers.register);
 
 
+router.post('/guardarUsuario', validation , usersControllers.guardarUsuario);
 
-router.get('/register', registerControllers.register);
-router.post('/guardarUsuario', registerControllers.guardarUsuario);
-router.get('/cuentas',registerControllers.cuentas)
-router.get('/login',registerControllers.login)
+router.get('/cuentas',usersControllers.cuentas)
 
+router.get('/login',usersControllers.login)
+
+/* router.get('/profile/:userId',usersControllers.cuentaId) */
 
 
 module.exports=router;
