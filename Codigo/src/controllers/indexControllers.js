0b15f38543b5a4e338fddc2a8f818fcf452b0cdc
jpmaqueda <jpmaqueda@gmail.com>
2023-02-04 20:19:17 +0000

    subiendo el generador de productos del index

--- Codigo/src/controllers/indexControllers.js
+++ Codigo/src/controllers/indexControllers.js
@@ -1,6 +1,13 @@
+const fs = require(`fs`)
+const path = require(`path`)
+const filepath = path.join(__dirname, "../data/productsDataBase.json")
+
+const productslectura = JSON.parse(fs.readFileSync(filepath, `utf-8`))
+
+
 const indexControllers={
     index:(req,res)=>{ 
-        res.render('index');
+        res.render('index',{productslectura});
     }
 }
 
