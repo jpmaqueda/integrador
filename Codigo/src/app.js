0c359c0de6f9e6a68d51e87603b34d81e88245a2
chisator <gianfrancosacchi@hotmail.com.ar>
2023-02-03 21:32:07 -0300

    Acomodando el app.js

--- Codigo/src/app.js
+++ Codigo/src/app.js
@@ -1,24 +1,33 @@
+/*          REQUIRE'S             */
 const express = require('express');
-const app = express();
-const path=require('path')
+const path=require('path');
+const methodOverride =  require('method-override');// Pasar poder usar los métodos PUT y DELETE
+const session =require('express-session');
 const PORT=3000;
-const methodOverride =  require('method-override');
-app.use(methodOverride('_method'));
 
+/*          EXPRESS               */
+const app = express();
+
+
+/*          MIDDLEWARES           */
+app.use(express.static(path.resolve(__dirname,'../public')));
+app.use(methodOverride('_method'));// Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
 app.use(express.urlencoded({ extended:false }));
 app.use(express.json());
+app.use(session({secret:'secreto!!!!'}))
 
+/*         TEMPLATE ENGINE        */
 app.set('view engine', 'ejs');
-app.set('views', path.join(__dirname,'/views'))
+app.set('views', path.join(__dirname,'/views'))// Define la ubicación de la carpeta de las Vistas
+
+
+/*         ROUTES                 */
+const products=require('./routes/products')
+const users=require('./routes/users')
+const indexRoutes=require('./routes/indexRoutes');
 
 app.listen(PORT,()=>console.log('Servidor corriendo en puerto '+ PORT))
-app.use(express.static(path.resolve(__dirname,'../public')));
 
-const indexRoutes=require('./routes/indexRoutes');
 app.use('/', indexRoutes)
-
-const users=require('./routes/users')
 app.use('/',users)
-
-const products=require('./routes/products')
 app.use('/', products)
